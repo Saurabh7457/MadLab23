3208f11a5608e72a8e97692f360afe0efe077d62
Saurabh7457 <126562453+Saurabh7457@users.noreply.github.com>
2023-05-14 00:40:27 +0530

    Create lab2_Q2.java

---
+++ lab2_Q2.java
@@ -0,0 +1,16 @@
+class Doctor{
+  public void treatPatient(){
+}
+class Surgeon extends Doctor{
+  public void treatPatient(){
+  }
+}
+Class run{
+  public static void main (String args[]){
+    Doctor doctorObj = new Doctor()
+    doctorObj.treatPatient();
+   
+    Surgeon surgeonObj = new Surgeon()
+    surgeonObj.treatPatient();
+  }
+}
